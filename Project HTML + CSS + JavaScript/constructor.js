491a1b42cfb9979ac5672afb87baf9491386a799
Jonathan Darmon <39027981+Darmon701@users.noreply.github.com>
2018-12-10 18:56:21 +0200

    Delete constructor.js

--- Project HTML + CSS + JavaScript/constructor.js
+++
@@ -1,7 +0,0 @@
-//function constructor
-function Task(taskText, taskDate, taskTime, stickyNoteId) {
-    this.taskText = taskText.value;
-    this.taskDate = taskDate.value;
-    this.taskTime = taskTime.value;
-    this.stickyNoteId = stickyNoteId;
-}
